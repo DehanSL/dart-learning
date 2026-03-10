d721b5864bc051de7a94812d3e684b7f3345097c
Dehan Kadushana <kadushana@gmail.com>
2026-03-10 12:49:51 +0530

    test the prefix and postfix

---
+++ day02/increment.dart
@@ -0,0 +1,35 @@
+void main(){
+  int a = 10;
+
+  print(a);
+
+
+
+print(  a=a+1);
+
+
+// prefix increment
+print(++a);
+
+// prefix decrement
+print(--a);
+
+
+print("-------------------");
+
+
+int b =20;
+print(b);
+
+// postfix increment
+print(b++);
+print(b);
+
+
+// postfix decrement
+print(b--);
+print(b);
+
+
+
+}
