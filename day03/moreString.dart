a1799c30500935a420affb08c741c9391f08476b
Dehan Kadushana <kadushana@gmail.com>
2026-03-14 14:02:38 +0530

    More String Methods

---
+++ day03/moreString.dart
@@ -0,0 +1,28 @@
+void main(){
+  String fName = "dehan";
+
+  print(fName.toUpperCase());
+
+
+  print("-------------------------------");
+
+
+  String lName = "KADUSHANA";
+  print(lName.toLowerCase());
+
+  print("-------------------------------");
+
+  print(fName.length);
+
+
+  print("-------------------------------");
+
+  String description = "My favorit programming language is Dart";
+  print(description.substring(24,28));
+
+
+  print("-------------------------------");
+
+  print(description.replaceAll("Dart", "Python"));
+
+}
