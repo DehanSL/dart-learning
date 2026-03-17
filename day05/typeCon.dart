f84c8111a0503454883ec6c0cbb9ebecf353d421
Dehan Kadushana <kadushana@gmail.com>
2026-03-17 16:04:43 +0530

    Type conversion

---
+++ day05/typeCon.dart
@@ -0,0 +1,33 @@
+void main(){
+  String sAge = "25";
+
+
+  int iAage = int.parse(sAge);
+  print(iAage);
+
+  double dAge = double.parse(sAge);
+  print(dAge);
+
+
+  print("------------------------------------");
+
+  int weight = 75;
+
+  print("Type of the $weight  lengt is:  ${weight.toString().runtimeType}");
+
+
+
+  double height = 150.50;
+
+  print("Type of $height is: ${height.toString().runtimeType}");
+
+
+
+
+  
+
+
+
+  
+
+}
