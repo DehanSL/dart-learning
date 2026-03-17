b073263b1047985d6373989bcf48c37dc02900ff
Dehan Kadushana <kadushana@gmail.com>
2026-03-17 20:33:18 +0530

    const data type

---
+++ day07/datTypeConst.dart
@@ -0,0 +1,25 @@
+void main (){
+
+  // centimeters to meters
+  double meter = 1;
+
+
+  const HmeterInCentimeter = 100;
+
+  double meterInCentimeter = meter*HmeterInCentimeter;
+
+  print("meter $meter = $meterInCentimeter centimeter");
+
+
+
+//miles to kilometer
+
+const km = 1.609;
+double miles = 2;
+
+double kilometer = miles*km;
+print(kilometer);
+
+
+
+}
